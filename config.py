bd68f882cefe513dbe7d71143c643c16d7b78d58
grenston <grenston.george@ecanarys.com>
2020-10-10 02:12:45 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-branch_name = "master" # Provide name of the branch
-signed_commit = True # Mark True/False if required signed commits as branch rule
-
-# Refer README.md for configuring other rules
+branch_name = "master"
+signed_commit = os.getenv(REQUIRED_SIGNED_COMMITS")
 branch_rules = { "required_approving_review_count":1,
                  "enforce_admins":True,
                  "dismiss_stale_reviews":True,
                  "user_push_restrictions":["User1","User2"],
                  "strict":False,
-                 "contexts":["Status_Check1","Status_Check2"]
+                 "contexts":["StatusCheck1","StatusCheck2"]
                  }
