372542ef250463690143fcd1568bc50c6676b200
Grenston George <grenston.ks@gmail.com>
2020-10-10 02:15:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 branch_name = "master"
-signed_commit = os.getenv(REQUIRED_SIGNED_COMMITS")
+signed_commit = True
 branch_rules = { "required_approving_review_count":1,
                  "enforce_admins":True,
                  "dismiss_stale_reviews":True,
